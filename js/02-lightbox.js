824aa55bfe9657af59aed2136a508e39572912b8
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-02-05 11:51:01 +0200

    add 2 hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEL = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}" >
+           <img class="gallery__image" src="${preview}" alt="${description}" "/>
+           </a>`;
+  })
+  .join("");
+
+galleryEL.innerHTML = galleryItemsEl;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
